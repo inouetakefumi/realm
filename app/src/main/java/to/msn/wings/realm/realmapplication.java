66ed7ed4925f83ccbd56322b5ea9b59f8b52b04d
inouetakefumi <janja1120@yahoo.co.jp>
2020-06-28 00:21:40 +0900

    Initial commit

---
+++ app/src/main/java/to/msn/wings/realm/realmapplication.java
@@ -0,0 +1,13 @@
+package to.msn.wings.realm;
+
+import android.app.Application;
+import io.realm.Realm;
+
+public class realmapplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+    }
+}
